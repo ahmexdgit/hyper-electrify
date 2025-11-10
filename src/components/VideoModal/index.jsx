8bf1118c9da57b65fab433c59917cee62592073d
ahmexdgit <humandesignrave@gmail.com>
2025-11-10 15:47:05 +0500

    removed react water wave

--- src/components/VideoModal/index.jsx
+++ src/components/VideoModal/index.jsx
@@ -1,27 +1,28 @@
-import React from "react";
-import { useState } from "react";
-import WaterWave from "react-water-wave";
+import React, { useState } from "react";
 import Spacing from "../Spacing";
 import parse from "html-react-parser";
 
 export default function VideoModal({ videoSrc, bgUrl, title, titleVariant }) {
   const [iframeSrc, setIframeSrc] = useState("about:blank");
   const [toggle, setToggle] = useState(false);
-  const handelClick = () => {
-    setIframeSrc(`${videoSrc}`);
+
+  const handleClick = () => {
+    setIframeSrc(videoSrc);
     setToggle(!toggle);
   };
-  const handelClose = () => {
+
+  const handleClose = () => {
     setIframeSrc("about:blank");
     setToggle(!toggle);
   };
+
   return (
     <>
-      {title ? (
+      {title && (
         <>
           <h2
             className={`cs_video_block_1_title cs_fs_68 text-center mb-0 ${
-              titleVariant ? titleVariant : "text-uppercase"
+              titleVariant || "text-uppercase"
             }`}
           >
             {parse(title)}
@@ -31,58 +32,30 @@ export default function VideoModal({ videoSrc, bgUrl, title, titleVariant }) {
             md="45"
           />
         </>
-      ) : (
-        ""
       )}
-      <WaterWave
-        className="cs_video_block cs_style_1 cs_bg_filed cs_radius_15 position-relative d-flex justify-content-center align-items-center cs_ripple_activate overflow-hidden"
-        imageUrl={bgUrl}
-      >
-        {() => (
-          <span
-            className="cs_hero_video_icon"
-            onClick={handelClick}
-          >
-            {/* <svg
-              width={80}
-              height={80}
-              viewBox="0 0 80 80"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle
-                cx={40}
-                cy={40}
-                r={40}
-                fill="#FD6219"
-              />
-              <path
-                d="M60.079 39.9998L30.148 57.4394L30.0104 22.7986L60.079 39.9998Z"
-                fill="white"
-              />
-            </svg> */}
-          </span>
-        )}
-      </WaterWave>
 
-      {/* <div className={toggle ? 'cs_video_popup active' : 'cs_video_popup'}>
-        <div className="cs_video_popup_overlay" />
-        <div className="cs_video_popup_content">
-          <div className="cs_video_popup_layer" />
-          <div className="cs_video_popup_container">
-            <div className="cs_video_popup_align">
-              <div className="embed-responsive embed-responsive-16by9">
-                <iframe
-                  className="embed-responsive-item"
-                  src={iframeSrc}
-                  title="video modal"
-                />
-              </div>
-            </div>
-            <div className="cs_video_popup_close" onClick={handelClose} />
-          </div>
-        </div>
-      </div> */}
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          overflow: "hidden",
+          borderRadius: "15px",
+        }}
+      >
+        <img
+          src={bgUrl}
+          alt="video background"
+          onClick={handleClick}
+          style={{
+            width: "100%",
+            height: "auto",
+            display: "block",
+            objectFit: "cover",
+            borderRadius: "15px",
+            cursor: "pointer",
+          }}
+        />
+      </div>
     </>
   );
 }
